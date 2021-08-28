e23e429a8a9fe704d7477a272770678af6552831
James Edward Gray II <james@graysoftinc.com>
2021-08-28 18:10:55 -0500

    Chapter 1.

---
+++ lib/light_touch/operations.ex
@@ -0,0 +1,34 @@
+defmodule LightTouch.Operations do
+  def add({lx, ly, lz, lw}, {rx, ry, rz, rw})
+      when lw in [0, 1] and rw in [0, 1] and lw + rw != 2 do
+    {lx + rx, ly + ry, lz + rz, lw + rw}
+  end
+
+  def cross_product({lx, ly, lz, 0}, {rx, ry, rz, 0}) do
+    {ly * rz - lz * ry, lz * rx - lx * rz, lx * ry - ly * rx, 0.0}
+  end
+
+  def divide({x, y, z, 0}, n) when is_number(n), do: {x / n, y / n, z / n, 0}
+
+  def dot_product({lx, ly, lz, 0}, {rx, ry, rz, 0}) do
+    lx * rx + ly * ry + lz * rz
+  end
+
+  def magnitude({x, y, z, 0}) do
+    :math.sqrt(:math.pow(x, 2) + :math.pow(y, 2) + :math.pow(z, 2))
+  end
+
+  def multiply({x, y, z, 0}, n) when is_number(n), do: {x * n, y * n, z * n, 0}
+
+  def negate({x, y, z, 0}), do: {-x, -y, -z, 0}
+
+  def normalize({x, y, z, 0} = vector) do
+    m = magnitude(vector)
+    {x / m, y / m, z / m, 0}
+  end
+
+  def subtract({lx, ly, lz, lw}, {rx, ry, rz, rw})
+      when lw in [0, 1] and rw in [0, 1] and lw + rw != -1 do
+    {lx - rx, ly - ry, lz - rz, lw - rw}
+  end
+end
---
+++ lib/light_touch/primitives.ex
@@ -0,0 +1,11 @@
+defmodule LightTouch.Primitives do
+  defguard is_point(term)
+           when is_tuple(term) and tuple_size(term) == 4 and elem(term, 3) == 1
+
+  defguard is_vector(term)
+           when is_tuple(term) and tuple_size(term) == 4 and elem(term, 3) == 0
+
+  def point(x, y, z), do: {x, y, z, 1}
+
+  def vector(x, y, z), do: {x, y, z, 0}
+end
