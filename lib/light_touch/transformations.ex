7d6020a8ac6de7b3e21b1fa9c4c366b9bda68196
James Edward Gray II <james@graysoftinc.com>
2021-09-10 16:46:47 -0500

    Chapter 4.

---
+++ lib/light_touch/transformations.ex
@@ -0,0 +1,89 @@
+defmodule LightTouch.Transformations do
+  alias LightTouch.{Primitives, Operations}
+
+  def rotate_x(degrees) do
+    radians = degrees_to_radians(degrees)
+
+    {
+      {1, 0, 0, 0},
+      {0, :math.cos(radians), -:math.sin(radians), 0},
+      {0, :math.sin(radians), :math.cos(radians), 0},
+      {0, 0, 0, 1}
+    }
+  end
+
+  def rotate_y(degrees) do
+    radians = degrees_to_radians(degrees)
+
+    {
+      {:math.cos(radians), 0, :math.sin(radians), 0},
+      {0, 1, 0, 0},
+      {-:math.sin(radians), 0, :math.cos(radians), 0},
+      {0, 0, 0, 1}
+    }
+  end
+
+  def rotate_z(degrees) do
+    radians = degrees_to_radians(degrees)
+
+    {
+      {:math.cos(radians), -:math.sin(radians), 0, 0},
+      {:math.sin(radians), :math.cos(radians), 0, 0},
+      {0, 0, 1, 0},
+      {0, 0, 0, 1}
+    }
+  end
+
+  def scale(x, y, z) do
+    {
+      {x, 0, 0, 0},
+      {0, y, 0, 0},
+      {0, 0, z, 0},
+      {0, 0, 0, 1}
+    }
+  end
+
+  def scale_x(x), do: scale(x, 1, 1)
+
+  def scale_y(y), do: scale(1, y, 1)
+
+  def scale_z(z), do: scale(1, 1, z)
+
+  def shear(xy, xz, yx, yz, zx, zy) do
+    {
+      {1, xy, xz, 0},
+      {yx, 1, yz, 0},
+      {zx, zy, 1, 0},
+      {0, 0, 0, 1}
+    }
+  end
+
+  def transform(transformations) do
+    Enum.reduce(
+      transformations,
+      Primitives.identity_matrix(),
+      fn {f, args}, combined ->
+        __MODULE__
+        |> apply(f, List.wrap(args))
+        |> Operations.multiply(combined)
+      end
+    )
+  end
+
+  def translate(x, y, z) do
+    {
+      {1, 0, 0, x},
+      {0, 1, 0, y},
+      {0, 0, 1, z},
+      {0, 0, 0, 1}
+    }
+  end
+
+  def translate_x(x), do: translate(x, 0, 0)
+
+  def translate_y(y), do: translate(0, y, 0)
+
+  def translate_z(z), do: translate(0, 0, z)
+
+  defp degrees_to_radians(degrees), do: degrees / 180 * :math.pi()
+end
---
+++ test/transformations_test.exs
@@ -0,0 +1,186 @@
+defmodule LightTouch.TransformationsTest do
+  use ExUnit.Case, async: true
+
+  import LightTouch.TestSupport.Comparisons
+  alias LightTouch.{Primitives, Operations, Transformations}
+
+  describe "translation" do
+    test "points" do
+      translation = Transformations.translate(5, -3, 2)
+      point = Primitives.point(-3, 4, 5)
+      translated = Operations.multiply(translation, point)
+      assert close_to(translated, Primitives.point(2, 1, 7))
+    end
+
+    test "inverse" do
+      translation =
+        Transformations.translate(5, -3, 2)
+        |> Operations.inverse()
+
+      point = Primitives.point(-3, 4, 5)
+      translated = Operations.multiply(translation, point)
+      assert close_to(translated, Primitives.point(-8, 7, 3))
+    end
+
+    test "vectors" do
+      translation = Transformations.translate(5, -3, 2)
+      vector = Primitives.vector(-3, 4, 5)
+      translated = Operations.multiply(translation, vector)
+      assert close_to(translated, vector)
+    end
+  end
+
+  describe "scaling" do
+    test "points" do
+      scale = Transformations.scale(2, 3, 4)
+      point = Primitives.point(-4, 6, 8)
+      scaled = Operations.multiply(scale, point)
+      assert close_to(scaled, Primitives.point(-8, 18, 32))
+    end
+
+    test "vectors" do
+      scale = Transformations.scale(2, 3, 4)
+      vector = Primitives.vector(-4, 6, 8)
+      scaled = Operations.multiply(scale, vector)
+      assert close_to(scaled, Primitives.vector(-8, 18, 32))
+    end
+
+    test "inverse" do
+      scale =
+        Transformations.scale(2, 3, 4)
+        |> Operations.inverse()
+
+      vector = Primitives.vector(-4, 6, 8)
+      scaled = Operations.multiply(scale, vector)
+      assert close_to(scaled, Primitives.vector(-2, 2, 2))
+    end
+
+    test "reflection" do
+      scale = Transformations.scale_x(-1)
+      point = Primitives.point(2, 3, 4)
+      scaled = Operations.multiply(scale, point)
+      assert close_to(scaled, Primitives.point(-2, 3, 4))
+    end
+  end
+
+  describe "rotation" do
+    test "points around the x axis" do
+      point = Primitives.point(0, 1, 0)
+      rotation = Transformations.rotate_x(45)
+      rotated = Operations.multiply(rotation, point)
+      root = :math.sqrt(2)
+      assert close_to(rotated, Primitives.point(0, root / 2, root / 2))
+
+      rotation = Transformations.rotate_x(90)
+      rotated = Operations.multiply(rotation, point)
+      assert close_to(rotated, Primitives.point(0, 0, 1))
+    end
+
+    test "inverse around the x axis" do
+      point = Primitives.point(0, 1, 0)
+
+      rotation =
+        Transformations.rotate_x(45)
+        |> Operations.inverse()
+
+      rotated = Operations.multiply(rotation, point)
+      root = :math.sqrt(2)
+      assert close_to(rotated, Primitives.point(0, root / 2, -root / 2))
+    end
+
+    test "points around the y axis" do
+      point = Primitives.point(0, 0, 1)
+      rotation = Transformations.rotate_y(45)
+      rotated = Operations.multiply(rotation, point)
+      root = :math.sqrt(2)
+      assert close_to(rotated, Primitives.point(root / 2, 0, root / 2))
+
+      rotation = Transformations.rotate_y(90)
+      rotated = Operations.multiply(rotation, point)
+      assert close_to(rotated, Primitives.point(1, 0, 0))
+    end
+
+    test "points around the z axis" do
+      point = Primitives.point(0, 1, 0)
+      rotation = Transformations.rotate_z(45)
+      rotated = Operations.multiply(rotation, point)
+      root = :math.sqrt(2)
+      assert close_to(rotated, Primitives.point(-root / 2, root / 2, 0))
+
+      rotation = Transformations.rotate_z(90)
+      rotated = Operations.multiply(rotation, point)
+      assert close_to(rotated, Primitives.point(-1, 0, 0))
+    end
+  end
+
+  describe "shearing" do
+    test "x in proportion to y" do
+      shear = Transformations.shear(1, 0, 0, 0, 0, 0)
+      point = Primitives.point(2, 3, 4)
+      sheared = Operations.multiply(shear, point)
+      assert close_to(sheared, Primitives.point(5, 3, 4))
+    end
+
+    test "x in proportion to z" do
+      shear = Transformations.shear(0, 1, 0, 0, 0, 0)
+      point = Primitives.point(2, 3, 4)
+      sheared = Operations.multiply(shear, point)
+      assert close_to(sheared, Primitives.point(6, 3, 4))
+    end
+
+    test "y in proportion to x" do
+      shear = Transformations.shear(0, 0, 1, 0, 0, 0)
+      point = Primitives.point(2, 3, 4)
+      sheared = Operations.multiply(shear, point)
+      assert close_to(sheared, Primitives.point(2, 5, 4))
+    end
+
+    test "y in proportion to z" do
+      shear = Transformations.shear(0, 0, 0, 1, 0, 0)
+      point = Primitives.point(2, 3, 4)
+      sheared = Operations.multiply(shear, point)
+      assert close_to(sheared, Primitives.point(2, 7, 4))
+    end
+
+    test "z in proportion to x" do
+      shear = Transformations.shear(0, 0, 0, 0, 1, 0)
+      point = Primitives.point(2, 3, 4)
+      sheared = Operations.multiply(shear, point)
+      assert close_to(sheared, Primitives.point(2, 3, 6))
+    end
+
+    test "z in proportion to y" do
+      shear = Transformations.shear(0, 0, 0, 0, 0, 1)
+      point = Primitives.point(2, 3, 4)
+      sheared = Operations.multiply(shear, point)
+      assert close_to(sheared, Primitives.point(2, 3, 7))
+    end
+  end
+
+  describe "chaining" do
+    test "applied in reverse order" do
+      point = Primitives.point(1, 0, 1)
+      rotation = Transformations.rotate_x(90)
+      rotated = Operations.multiply(rotation, point)
+      assert close_to(rotated, Primitives.point(1, -1, 0))
+
+      scale = Transformations.scale(5, 5, 5)
+      scaled = Operations.multiply(scale, rotated)
+      assert close_to(scaled, Primitives.point(5, -5, 0))
+
+      translation = Transformations.translate(10, 5, 7)
+      translated = Operations.multiply(translation, scaled)
+      assert close_to(translated, Primitives.point(15, 0, 7))
+
+      transformation =
+        Transformations.transform(
+          rotate_x: 90,
+          scale: [5, 5, 5],
+          translate: [10, 5, 7]
+        )
+
+      transformed = Operations.multiply(transformation, point)
+      assert close_to(transformed, Primitives.point(15, 0, 7))
+    end
+  end
+end
